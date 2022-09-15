3bc2bde4c5a6bd08902b0b414bd13289ada4d256
Spit <95194961+spit-dev@users.noreply.github.com>
2022-09-14 21:13:50 -0500

    Update

--- dikz_payloads/deps.py
+++ dikz_payloads/deps.py
@@ -6,6 +6,12 @@ import shutil
 import socket
 import subprocess
 import threading
+import ctypes
+import requests
 import base64
 import winreg as reg
 from colorama import Fore, init, Back
+import winshell
+import win32com.client
+import win32api, win32con
+
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import os
+import sys
 import time
-from dikz_funcs import *
+import glob
+import shutil
+import socket
+import subprocess
+import threading
+import ctypes
+import requests
+import base64
+import winreg as reg
 from colorama import Fore, init, Back
+import winshell
+import win32com.client
+import win32api, win32con
+from dikz_funcs import *
+
 os_comp()
 init()
 cls()
